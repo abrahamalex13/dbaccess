3921213c1deff45d197f7e183c28d484889bd082
Alex Abraham <alex.abraham13@gmail.com>
2022-10-09 08:46:33 -0500

    Format

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,35 +6,37 @@ import dbaccess as dbac
 
 load_dotenv()
 
+
 @pytest.fixture(scope="session")
 def aws_rds_specs0():
 
     db_specs = dbac.DBSpecs(
-        cloud_platform='aws_rds'
-        , profile_name='default'
-        , endpoint=os.environ.get('ENDPOINT_DB')
-        , port=os.environ.get('PORT_DB')
-        , region=os.environ.get('REGION_DB')
-        , db_name=os.environ.get('NAME_DB')
-        , db_user=os.environ.get('USER_DB')
-        , engine_name='postgresql'
-        )
+        cloud_platform="aws_rds",
+        profile_name="default",
+        endpoint=os.environ.get("ENDPOINT_DB"),
+        port=os.environ.get("PORT_DB"),
+        region=os.environ.get("REGION_DB"),
+        db_name=os.environ.get("NAME_DB"),
+        db_user=os.environ.get("USER_DB"),
+        engine_name="postgresql",
+    )
 
     return db_specs
+
 
 @pytest.fixture(scope="session")
 def aws_rds_specs(aws_rds_specs0):
     return aws_rds_specs0.format()
 
+
 @pytest.fixture(scope="session")
 def aws_rds_access_authenticator(aws_rds_specs):
     return dbac.DBAccessAuthenticator(aws_rds_specs).authenticate()
 
+
 @pytest.fixture(scope="session")
 def pg_db_access(aws_rds_access_authenticator):
 
-    db_access = dbac.DBAccess(
-        aws_rds_access_authenticator
-        ).connect()
+    db_access = dbac.DBAccess(aws_rds_access_authenticator).connect()
 
     return db_access

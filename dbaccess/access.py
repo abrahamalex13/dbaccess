0faf415d5417e9fd8b97d47d95a5522b27d1cbce
abrahamalex13 <alex.abraham13@gmail.com>
2022-10-17 20:22:44 -0500

    From `access`, call `authenticator`

--- dbaccess/access.py
+++ dbaccess/access.py
@@ -1,27 +1,25 @@
-import boto3
 import sqlalchemy
+from dbaccess import get_authenticator
 
-KEYWORDS_GENERATE_AUTH_TOKEN = [
-    "AWS_RDS_IAM_TOKEN"
+KEYWORDS_GENERATE_AUTH_TOKEN = ["AWS_RDS_IAM_TOKEN"]
+KEYS_DB_SPECS_URL = [
+    'drivername', 'username', 'password', 'host'
+    , 'port', 'database', 'query'
 ]
 
-def create_engine_from_specs(db_specs):
+def create_engine_from_specs(db_specs, service_client_specs=None):
 
     need_auth_token = db_specs["password"] in KEYWORDS_GENERATE_AUTH_TOKEN
 
     if need_auth_token:
+        authenticator = get_authenticator(db_specs)(db_specs, service_client_specs)
+        authenticator.authenticate()
+        db_specs['password'] = authenticator.token
 
-        session = boto3.Session()
-        client = session.client(service_name='rds', region_name=db_specs['region'])
-        db_specs['password'] = client.generate_db_auth_token(
-            DBHostname=db_specs['host']
-            , Port=db_specs['port']
-            , DBUsername=db_specs['username']
-            , Region=db_specs['region']
-        )
-        del db_specs['region']
-
-    url_engine = sqlalchemy.engine.URL.create(**db_specs)
+    db_specs_url = {}
+    for key in KEYS_DB_SPECS_URL:
+        db_specs_url[key] = db_specs[key]
+    url_engine = sqlalchemy.engine.URL.create(**db_specs_url)
 
     engine = sqlalchemy.create_engine(url_engine)
 

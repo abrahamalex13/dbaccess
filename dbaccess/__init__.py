8aee5d6f52969d08484eeea9ace9a262c6ffeb93
abrahamalex13 <alex.abraham13@gmail.com>
2022-10-17 21:00:52 -0500

    Rename `authenticator` module

--- dbaccess/__init__.py
+++ dbaccess/__init__.py
@@ -1,3 +1,2 @@
-from dbaccess.specs import DBSpecs, AWSRDSSpecs
-from dbaccess.authenticator import get_authenticator, AWSRDSAccessAuthenticator
+from dbaccess.authenticate import get_authenticator, AWSRDSAccessAuthenticator
 from dbaccess.access import create_engine_from_specs

c747aad1816c4065d2791bc3f38beae42b4a3e71
abrahamalex13 <alex.abraham13@gmail.com>
2022-10-17 20:25:57 -0500

    Revise import names

--- dbaccess/__init__.py
+++ dbaccess/__init__.py
@@ -1,3 +1,3 @@
 from dbaccess.specs import DBSpecs, AWSRDSSpecs
-from dbaccess.authenticator import DBAccessAuthenticator, AWSRDSAccessAuthenticator
+from dbaccess.authenticator import get_authenticator, AWSRDSAccessAuthenticator
 from dbaccess.access import create_engine_from_specs

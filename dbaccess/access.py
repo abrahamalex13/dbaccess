698383fae0acade6fcf33450eb08b132880da9a4
abrahamalex13 <alex.abraham13@gmail.com>
2022-10-23 08:23:42 -0500

    Implement `_details` naming in access

--- dbaccess/access.py
+++ dbaccess/access.py
@@ -2,21 +2,21 @@ import sqlalchemy
 from dbaccess import get_authenticator
 
 KEYWORDS_GENERATE_AUTH_TOKEN = ["AWS_RDS_IAM_TOKEN"]
-KEYS_DB_SPECS_URL = [
+KEYS_DB_ARGS_URL = [
     'drivername', 'username', 'password', 'host'
     , 'port', 'database', 'query'
 ]
 
-def create_engine_from_specs(db_specs, service_client_specs=None):
+def create_engine_from_details(db_details, service_client_details=None):
     """
-    Create a `sqlalchemy` connection engine from database specification details. 
+    Create a `sqlalchemy` connection engine from database implementation details. 
     Do so by: automating construction of engine URL, 
-    generating connection auth token (if prompted by db_specs password).
+    generating connection auth token (if prompted by db password).
 
     Parameters
     ----------
-    db_specs : dict
-        Dict of database specification keywords & values.
+    db_details : dict
+        Dict of database implementation details keywords & values.
         Used to construct `sqlalchemy` connection engine URL
         (authenticating connection if prompted).
         Example keywords include: drivername, username, password, host, port, 
@@ -24,7 +24,7 @@ def create_engine_from_specs(db_specs, service_client_specs=None):
         
         Prompt auth token generation via `password` key: AWS_RDS_IAM_TOKEN.
 
-    service_client_specs : dict
+    service_client_details : dict
         Dict of [cloud computing] service client keywords & values.
         Used to instantiate service client session,
         through commands from a package like `boto3`. 
@@ -32,17 +32,17 @@ def create_engine_from_specs(db_specs, service_client_specs=None):
 
     """
 
-    need_auth_token = db_specs["password"] in KEYWORDS_GENERATE_AUTH_TOKEN
+    need_auth_token = db_details["password"] in KEYWORDS_GENERATE_AUTH_TOKEN
 
     if need_auth_token:
-        authenticator = get_authenticator(db_specs)(db_specs, service_client_specs)
+        authenticator = get_authenticator(db_details)(db_details, service_client_details)
         authenticator.authenticate()
-        db_specs['password'] = authenticator.token
+        db_details['password'] = authenticator.token
 
-    db_specs_url = {}
-    for key in KEYS_DB_SPECS_URL:
-        db_specs_url[key] = db_specs[key]
-    url_engine = sqlalchemy.engine.URL.create(**db_specs_url)
+    db_details_url = {}
+    for key in KEYS_DB_ARGS_URL:
+        db_details_url[key] = db_details[key]
+    url_engine = sqlalchemy.engine.URL.create(**db_details_url)
 
     engine = sqlalchemy.create_engine(url_engine)
 
